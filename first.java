278582202e42d1f12d225a95540e99c7699ed3e7
sharmistha412 <dassharmistha412@gmail.com>
2021-10-03 08:22:34 +0530

    minor changes

--- first.java
+++ first.java
@@ -1,12 +1,5 @@
 class main{
-<<<<<<< HEAD
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Good Night");
-=======
     public static void pp(String[] args) {
         System.out.println("Hello World!");
->>>>>>> temp
-    }
-    
+    } 
 }
